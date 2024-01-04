99ac7e80d01ac5299946d61868710823049061fa
Sujin <sunilkumarsujin@gmail.com>
2024-01-04 08:34:33 +0530

    till video title

--- netflix-gpt/src/components/Browse.js
+++ netflix-gpt/src/components/Browse.js
@@ -1,11 +1,17 @@
-import React, { useEffect } from 'react'
-import Header from './Header'
-import { API_OPTIONS } from '../utils/constant'
-import { useDispatch } from 'react-redux'
-import { addNowPlayingMovies } from '../utils/movieslice'
+import React from 'react'
+import Header from './Header.js'
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { addNowPlayingMovies } from "../utils/movieslice"
+import { API_OPTIONS } from '../utils/constant.js'
+import MainContainer from './MainContainer.js';
+import SecondaryContainer from './SecondaryContainer.js';
+
+
 
 const Browse = () => {
-
+ 
+  
   const dispatch = useDispatch()
   
   const getNowPlayingMovies = async () => {
@@ -14,14 +20,29 @@ const Browse = () => {
     console.log(json.results)
     dispatch(addNowPlayingMovies(json.results))
   }
-
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
     getNowPlayingMovies();
   },[])
 
+
   return (
+
     <div>
     <Header/>
+    <MainContainer/>
+    <SecondaryContainer/> 
+    {
+      /*
+        main container
+          - videoBackground
+          - video title
+
+        SecondaryContainer
+          - movielists * n
+            - cards * n
+       */
+    }
 
     </div>
   )
